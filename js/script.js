2aa2e453cfdcfb0438d28424479af55205ea6b17
vethilm <burnsc376@gmail.com>
2024-01-21 15:52:19 -0500

    layout

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function mobileNav() {
+    let x = document.getElementById("menus");
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function navIcon(x) {
+    x.classList.toggle("fa-minus-circle");
+}
